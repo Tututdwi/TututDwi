167e7097215454453e3f2cc5d20d3dcbac3b9ae3
Tututdwi <43515022+Tututdwi@users.noreply.github.com>
2018-09-24 19:22:09 +0700

    Add files via upload

---
+++ tutut/tugas pernikahan/src/tugas/pernikahan/TugasPernikahan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.pernikahan;
+
+/**
+ *
+ * @author Titin
+ */
+public class TugasPernikahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 20;
+        boolean cantik = true;
+        if (umur > 23 && cantik == true ){
+            System.out.println("MAU BANGET");
+        }
+        else if (umur > 23){
+            System.out.println("MIKIR DULU DEH");
+        }
+        else {
+            System.out.println("NO WAYYY !!!!");
+        }
+    }
+    
+}
